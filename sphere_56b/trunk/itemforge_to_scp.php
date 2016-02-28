af0c479432f36deb2365c64e25bfd586d0fdd8ba
Vinicius Arroyo <vinicius.arroyo@gmail.com>
2016-02-27 22:31:41 -0300

    Ferramenta para exportar dados do itemforge convertida para UTF-8. Não gera arquivo vazio se falhar conexão com o banco. Dados do itemforge atualizados

--- sphere_56b/trunk/itemforge_to_scp.php
+++ sphere_56b/trunk/itemforge_to_scp.php
@@ -1,5 +1,7 @@
 <?
 
+//TODO: work on a tempfile to avoid corruption
+
 function fileOpen(){
 	global $fp;
 
@@ -14,7 +16,6 @@ function fileClose(){
 
 function writeln( $ln ){
 	global $fp;
-
 //	echo "$ln\n";
 	fwrite( $fp,"$ln\n" );
 }
@@ -27,9 +28,9 @@ function writeMultiLine($token,$data){
 			continue;
 
 		if( $token )
-			writeln( "$token=$v" );
+			writeUtf8( "$token=$v" );
 		else
-			writeln( "$v" );
+			writeUtf8( "$v" );
 	}
 }
 
@@ -54,45 +55,49 @@ function writeSectionHeader($cat,$sec){
 	writeln("");
 }
 
+function writeUtf8($s){
+	writeln( utf8_encode( $s ) );
+}
+
 function writeItem($DB){
-	writeln("//*****************************************************************************");
-	writeln("//Item: ".$DB->f('itemdef') );
-	writeln("//*****************************************************************************");
-	writeln("//Rev: ".$DB->f('revision')." Ts: ".$DB->f('timestamp') );
-	writeln("[ITEMDEF ".$DB->f('itemdef')."]");
-	writeln("ID=".$DB->f('dispid'));
-	writeln("NAME=".$DB->f('name'));
-	writeln("CATEGORY=".$DB->f('category'));
-	writeln("SUBSECTION=".$DB->f('subsection'));
-	writeln("DESCRIPTION=".$DB->f('name'));
-	writeln("WEIGHT=".$DB->f('weight'));
-	writeln("VALUE=".$DB->f('value'));
-	writeln("TYPE=".$DB->f('type'));
-	writeln("RESOURCES=".$DB->f('resources'));
-	writeln("SKILLMAKE=".$DB->f('skillmake'));
-	writeln("LAYER=".$DB->f('layer'));
-	writeln("PILE=".$DB->f('pile'));
-	writeln("TDATA1=".$DB->f('tdata1'));
-	writeln("TDATA2=".$DB->f('tdata2'));
-	writeln("TDATA3=".$DB->f('tdata3'));
-	writeln("TDATA4=".$DB->f('tdata4'));
+	writeUtf8("//*****************************************************************************");
+	writeUtf8("//Item: ".$DB->f('itemdef') );
+	writeUtf8("//*****************************************************************************");
+	writeUtf8("//Rev: ".$DB->f('revision')." Ts: ".$DB->f('timestamp') );
+	writeUtf8("[ITEMDEF ".$DB->f('itemdef')."]");
+	writeUtf8("ID=".$DB->f('dispid'));
+	writeUtf8("NAME=".$DB->f('name'));
+	writeUtf8("CATEGORY=".$DB->f('category'));
+	writeUtf8("SUBSECTION=".$DB->f('subsection'));
+	writeUtf8("DESCRIPTION=".$DB->f('name'));
+	writeUtf8("WEIGHT=".$DB->f('weight'));
+	writeUtf8("VALUE=".$DB->f('value'));
+	writeUtf8("TYPE=".$DB->f('type'));
+	writeUtf8("RESOURCES=".$DB->f('resources'));
+	writeUtf8("SKILLMAKE=".$DB->f('skillmake'));
+	writeUtf8("LAYER=".$DB->f('layer'));
+	writeUtf8("PILE=".$DB->f('pile'));
+	writeUtf8("TDATA1=".$DB->f('tdata1'));
+	writeUtf8("TDATA2=".$DB->f('tdata2'));
+	writeUtf8("TDATA3=".$DB->f('tdata3'));
+	writeUtf8("TDATA4=".$DB->f('tdata4'));
 
 	if( strlen($DB->f('dam')) > 0 )
-		writeln("DAM=".$DB->f('dam'));
+		writeUtf8("DAM=".$DB->f('dam'));
 	else if( strlen($DB->f('armor')) > 0 )
-		writeln("ARMOR=".$DB->f('armor'));
+		writeUtf8("ARMOR=".$DB->f('armor'));
 
-	writeln("SPEED=".$DB->f('speed'));
-	writeln("SKILL=".$DB->f('skill'));
-	writeln("REQSTR=".$DB->f('reqstr'));
-	writeln("TWOHANDS=".$DB->f('twohands'));
-//	writeln("TEVENTS=".$DB->f('tevents'));
+	writeUtf8("SPEED=".$DB->f('speed'));
+	writeUtf8("SKILL=".$DB->f('skill'));
+	writeUtf8("REQSTR=".$DB->f('reqstr'));
+	writeUtf8("TWOHANDS=".$DB->f('twohands'));
+//	writeUtf8("TEVENTS=".$DB->f('tevents'));
 	writeMultiLine("TEVENTS",$DB->f('tevents'));
-	writeln("");
-	writeln("ON=@CREATE");
-	writeln("COLOR=".$DB->f('color'));
+	writeUtf8("");
+	writeUtf8("ON=@CREATE");
+	writeUtf8("COLOR=".$DB->f('color'));
 	writeMultiLine( "",$DB->f('create'));
-	writeln("");
+	writeUtf8("");
 }
 
 require("mysql.php");
@@ -105,10 +110,12 @@ $section = "";
 $cnt = 0;
 $fp = 0;
 
+
+$DB->query("SELECT *, FORMAT(weight/10,1) AS weight FROM itemforge WHERE valid=1 ORDER BY category,subsection,name");
+
 fileOpen();
 writeFileHeader();
 
-$DB->query("SELECT *, FORMAT(weight/10,1) AS weight FROM itemforge WHERE valid=1 ORDER BY category,subsection,name");
 while( $DB->next_record() ){
 
 	if( $DB->f('category') != $category  || $DB->f('subsection') != $section ){
